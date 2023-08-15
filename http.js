1a4eccaf45f9cc017e5315cfbb9adb985236c6fd
[Temitope Harry] <tooyero@student.oauife.edu.ng>
2023-08-15 12:12:23 +0100

    second commit

--- http.js
+++ http.js
@@ -1,2 +1,18 @@
-const http = require('http');
-console.log(http)
+const http = require("http");
+const { EOL } = require("os");
+const server = http.createServer((req, res) => {
+  // console.log(req.url)
+  // res.writeHead(200,{'content-type': 'text/html'})
+  // res.write("Hello World From Server Side")
+  // res.write('</br>')
+  // res.write("<i>Server Side Programming Is Cool</i>");
+  const { url } = req;
+  if (url == "/") res.end("<h1>Home Page</h1>");
+  if (url == "/about") res.end("<h1>About Page</h1>");
+  if (url == "/contact") res.end("<h1>Contact Page</h1>");
+  // res.end('<h1>Hello <i>World</i></h1>')
+});
+server.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
+// console.log(http)
---
+++ url.js
@@ -0,0 +1,3 @@
+const url = require('url')
+'http://www.Alusofttechnologies.com/learn2code/register'
+console.log(url)
