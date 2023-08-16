43c80ebd1eaead63d1473f63503f3688c6cb1cd5
[Temitope Harry] <tooyero@student.oauife.edu.ng>
2023-08-16 14:07:12 +0100

    update2

--- http.js
+++ http.js
@@ -1,4 +1,6 @@
+const fs = require("fs");
 const http = require("http");
+const {resolve} = require('path')
 const { EOL } = require("os");
 const server = http.createServer((req, res) => {
   // console.log(req.url)
@@ -7,10 +9,45 @@ const server = http.createServer((req, res) => {
   // res.write('</br>')
   // res.write("<i>Server Side Programming Is Cool</i>");
   const { url } = req;
-  if (url == "/") res.end("<h1>Home Page</h1>");
-  if (url == "/about") res.end("<h1>About Page</h1>");
-  if (url == "/contact") res.end("<h1>Contact Page</h1>");
-  // res.end('<h1>Hello <i>World</i></h1>')
+  if (url === "/") {
+    try {
+      const home = fs.readFileSync(resolve('resources','pages','home.html'), 'utf-8')
+      res.end(home)
+    } catch (error) {
+      res.writeHead(404, {'content-type': 'text/html'} )
+      res.end('<h1>Server Not Found</h1>')
+    }
+  }
+  else if (url === "/style.css") {
+    try {
+      const home = fs.readFileSync(resolve('resources','CSS','style.css'), 'utf-8')
+      res.end(home)
+    } catch (error) {
+      res.writeHead(404, {'content-type': 'text/css'} )
+      res.end('<h1>Server Not Found</h1>')
+    }
+  }
+  else if (url == "/about") {
+    try {
+      const abt = fs.readFileSync(resolve('resources','pages','about.html'))
+      res.end(abt)
+    } catch (error) {
+      res.writeHead(404,"Error", {'content-type':'text/html'})
+      res.end("Server not found")
+    }
+  }
+  else if (url == "/contact") {
+    try {
+      const cont = fs.readFileSync(resolve('resources','pages','contact.html'));
+      res.end(cont)
+    } catch (error) {
+      res.writeHead(500, "Error, Page not Found", {'content-type':'text/html'})
+    }
+    res.end('Error')
+  }
+  else {
+    res.writeHead(404, {'content-type': 'text/html'})
+    res.end('<h1>Page not Found</h1>')}
 });
 server.listen(3000, () => {
   console.log("server listening on port 3000");
--- path.js
+++ path.js
@@ -1,14 +1,15 @@
 const math = require('./index')
 console.log(math.Sum(1,2,3) + math.prod(1,2,6))
 console.log(math.Factorial(5))
-const {resolve, normalize, isAbsolute, relative, join,extname} = require('path')
+const {resolve} = require('path')
 // isAbsolute()
 // console.log(path)
-console.log(extname(''))
-console.log(__dirname)
-console.log(__filename)
+// console.log(extname(''))
+// console.log(__dirname)
+// console.log(__filename)
 
 function setTime(){
     console.log('Hello World')
     
 }
+module.exports= {resolve}
